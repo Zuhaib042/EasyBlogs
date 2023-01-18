0fbf47e8f022d47525185a6f7924464eebb31cba
Zuhaib Amjad <umar0423@gmail.com>
2023-01-18 18:41:04 +0500

    Set up associations in User and Post Models correctly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', counter_cache: true
+  has_many :likes, counter_cache: true
+  has_many :comments, counter_cache: true
 end
