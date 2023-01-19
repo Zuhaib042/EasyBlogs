92da0f4aec47160bd722aa8b0dc66a25555d0efd
Zuhaib Amjad <umar0423@gmail.com>
2023-01-19 14:39:03 +0500

    Implementing methods in comment and like classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :post, counter_cache: true
-  belongs_to :author, class_name: 'User', counter_cache: true
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_comments_counter
+  
+  private 
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
