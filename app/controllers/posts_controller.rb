5faa3edd5dbde45c3b11a2fab48d05634e06c558
Zuhaib Amjad <umar0423@gmail.com>
2023-01-27 19:04:27 +0500

    Add new and create action methods in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.where(id: params[:id], author_id: params[:user_id]).first
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(**post_params, author_id: current_user.id)
+    @user = current_user
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+ 
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
