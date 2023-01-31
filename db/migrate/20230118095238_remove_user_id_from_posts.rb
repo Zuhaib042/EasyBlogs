83758087c8521a81d2225e6c688ed0fd55a83e8f
Zuhaib Amjad <umar0423@gmail.com>
2023-01-31 20:00:14 +0500

    Add and configure devise for authentication

--- db/migrate/20230118095238_remove_user_id_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromPosts < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :posts, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_053831) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_111323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_053831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "text"
-    t.bigint "post_id", null: false
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_053831) do
     t.string "photo"
     t.text "bio"
     t.integer "posts_counter", default: 0
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
