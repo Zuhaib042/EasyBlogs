9dcd4e7ab62c9bcebdf66f3c355373402c8bccf1
Zuhaib Amjad <umar0423@gmail.com>
2023-01-31 09:02:06 +0500

    Fix linters errors

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -2,30 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'User show method ', type: :feature do
   before(:example) do
-    @user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80', bio: 'Teacher from Poland.', posts_counter: 10)
-    
-    @posts = [ Post.create(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0,
-    comments_counter: 0),
-     Post.create(author: @user, title: 'Hi', text: 'This is my second post', likes_counter: 0,
-    comments_counter: 0),
-     Post.create(author: @user, title: 'What"s up', text: 'This is my third post', likes_counter: 0, comments_counter: 0)
-     ]
-    
+    @user = User.create(name: 'Lilly',
+                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80', bio: 'Teacher from Poland.', posts_counter: 10)
+
+    @posts = [Post.create(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0,
+                          comments_counter: 0),
+              Post.create(author: @user, title: 'Hi', text: 'This is my second post', likes_counter: 0,
+                          comments_counter: 0),
+              Post.create(author: @user, title: 'What"s up', text: 'This is my third post', likes_counter: 0,
+                          comments_counter: 0)]
+
     @comments = [
-     Comment.create(author: @user, post: @posts[0], text: 'Hi Tom!' ),
-     Comment.create(author: @user, post: @posts[0], text: 'How are you' ),
-     Comment.create(author: @user, post: @posts[1], text: 'What are you doing tonight')
+      Comment.create(author: @user, post: @posts[0], text: 'Hi Tom!'),
+      Comment.create(author: @user, post: @posts[0], text: 'How are you'),
+      Comment.create(author: @user, post: @posts[1], text: 'What are you doing tonight')
     ]
-        
+
     visit user_posts_path(@user, @posts)
   end
-  
+
   it 'shows the user"s profile picture' do
     expect(page.html).to include('user-photo')
     find("img[src='#{@user.photo}']")
   end
-  
-  it "shows the name of username" do
+
+  it 'shows the name of username' do
     expect(page).to have_content(@user.name)
   end
 
@@ -35,13 +36,13 @@ RSpec.describe 'User show method ', type: :feature do
 
   it 'shows a post title' do
     @posts.each do |post|
-      expect(page).to have_content(post.title) 
+      expect(page).to have_content(post.title)
     end
   end
-  
+
   it 'shows a post body' do
     @posts.each do |post|
-      expect(page).to have_content(post.text) 
+      expect(page).to have_content(post.text)
     end
   end
 
@@ -68,5 +69,4 @@ RSpec.describe 'User show method ', type: :feature do
     click_link(post.title)
     expect(page).to have_current_path(user_post_path(@user, post))
   end
-  
 end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'User show method ', type: :feature do
   before(:example) do
-    @user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80', bio: 'Teacher from Poland.', posts_counter: 10)
-    
-    @posts = [ Post.create(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0,
-    comments_counter: 0),
-     Post.create(author: @user, title: 'Hi', text: 'This is my second post', likes_counter: 0,
-    comments_counter: 0),
-     Post.create(author: @user, title: 'What"s up', text: 'This is my third post', likes_counter: 0, comments_counter: 0)
-    ]
+    @user = User.create(name: 'Lilly',
+                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80', bio: 'Teacher from Poland.', posts_counter: 10)
+
+    @posts = [Post.create(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0,
+                          comments_counter: 0),
+              Post.create(author: @user, title: 'Hi', text: 'This is my second post', likes_counter: 0,
+                          comments_counter: 0),
+              Post.create(author: @user, title: 'What"s up', text: 'This is my third post', likes_counter: 0,
+                          comments_counter: 0)]
     visit user_path(@user)
   end
-
 
   it 'shows the user"s profile picture' do
     expect(page.html).to include('user-photo')
     find("img[src='#{@user.photo}']")
   end
-  
-  it "shows the name of username" do
+
+  it 'shows the name of username' do
     expect(page).to have_content(@user.name)
   end
 
@@ -30,7 +30,7 @@ RSpec.describe 'User show method ', type: :feature do
   it 'shows the user\'s bio' do
     expect(page).to have_content(@user.bio)
   end
-  
+
   it 'shows the user\'s first three posts' do
     @posts.each do |post|
       expect(page).to have_content(post.title)
@@ -38,7 +38,7 @@ RSpec.describe 'User show method ', type: :feature do
   end
 
   it 'shows the "see all posts" button' do
-    find_button("See all Posts")
+    find_button('See all Posts')
   end
 
   it 'redirects to the the clicked post link' do
@@ -48,7 +48,7 @@ RSpec.describe 'User show method ', type: :feature do
   end
 
   it 'redirects to "posts" link' do
-    click_link("See all Posts")
+    click_link('See all Posts')
     expect(page).to have_current_path(user_posts_path(@user))
   end
 end
