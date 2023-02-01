9a1b345aac3616e8a242dbbc037e3fc8f0cdb4e1
Zuhaib Amjad <umar0423@gmail.com>
2023-02-01 17:02:01 +0500

    Update schema to add default photo and add confirmation columns in devise migration

---
+++ db/migrate/20230201083731_update_users_table.rb
@@ -0,0 +1,5 @@
+class UpdateUsersTable < ActiveRecord::Migration[7.0]
+  def change
+    change_column :users, :photo, :string, null: true, default: "https://cdn.dribbble.com/users/9685/screenshots/997495/avatarzzz.gif"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_111323) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_083731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_111323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "photo"
+    t.string "photo", default: "https://cdn.dribbble.com/users/9685/screenshots/997495/avatarzzz.gif"
     t.text "bio"
     t.integer "posts_counter", default: 0
     t.string "email", default: "", null: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_111323) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
