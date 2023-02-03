6745f2c34dbd3f65ceeba57726f82793caa9bba9
Zuhaib Amjad <umar0423@gmail.com>
2023-02-03 18:18:25 +0500

    Add the routes for api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+  
   resources :users, only: [:index, :show]  do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
+    end  
+  end
+
+  # api endpoints routes
+  namespace :api do
+    namespace :v1 do
+      resources :users , only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
 end
